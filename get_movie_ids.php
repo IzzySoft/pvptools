afd1f82bd197ceda5cdf17c5a21b4156658fff83
Izzy <izzysoft@qumran.org>
2009-01-16 14:29:57 +0000

    * initial check-in of the pvptools scripts and documents (v0.2)
    git-svn-id: svn+ssh://svn.izzysoft.de/public/phpvideopro/pvptools@2546 4b860ef2-653a-0410-b28f-a1f29e8e3dd0

---
+++ get_movie_ids.php
@@ -0,0 +1,181 @@
+#!/usr/bin/php
+<?
+#==============================================================================
+# phpVideoPro IMDB Updater                     (c) 2007-2009 by Itzchak Rehberg
+# Pass the name of the installation as first argument
+# -----------------------------------------------------------------------------
+# $Id$
+#==============================================================================
+
+error_reporting(E_ERROR); # disable all but errors to be reported
+
+#==========================================================[ Configuration ]===
+ $instance["demo"]->dir = '/var/www/phpvideo';
+ $instance["demo"]->adminID = 1;
+ $instance["demo"]->imdbid  = TRUE; // scan for imdb ids?
+ $instance["demo"]->rating  = TRUE; // update ratings?
+
+ $stopafter   = 200; // process no more than 200 entries at once
+ $ignore_cat  = "cat_videoclip"; // list of cat_id to ignore - like VideoClips, comma-separated
+ $skip_id     = array();
+ $skip_to     = 1; // start at the beginning or other given index
+ $stop_at     = 100;
+
+ # DB, when...
+ $write_yt     = TRUE; // 100% match on title and year
+ $write_aka_yt = TRUE; // 100% match on title and year for AKA
+ $write_yf     = FALSE; // 100% match on year, take 1st entry
+ $write_yd     = FALSE; // 100% match on year and director
+
+#=========================================================[ Initialization ]===
+ if (empty($instance[$argv[1]]->dir)) { // no such installation
+   die("Sorry - but we have no instance with the name '".$argv[1]."'.\n");
+ } else {
+   $cleaninst = $instance[$argv[1]];
+   echo "Updating PVP Installation '".$argv[1]."':\n";
+ }
+ if (file_exists($cleaninst->dir)) chdir($cleaninst->dir);
+ else die ("Sorry - but the given directory does not exist: '".$cleaninst->dir."'.\n");
+ $pvp->auth->user_id = $cleaninst->adminID;
+ $pvpinstall = 1;
+ if (file_exists("inc/includes.inc")) {
+   include ("inc/config.inc");
+   include ("inc/config_internal.inc");
+   include ("inc/class.preferences.inc");
+   $pvp->preferences = new preferences;
+   $incpath = explode(":", ini_get('include_path'));
+   $IMDBfound = FALSE;
+   foreach($incpath as $trypath) {
+     if (file_exists("$trypath/imdb.class.php")) {
+       echo "Including IMDB class\n";
+       include_once("$trypath/imdb.class.php");
+       $IMDBfound = TRUE;
+       break;
+     }
+   }
+   if (!$IMDBfound) die("IMDB classes not found, aborting.\n");
+ } else {
+   die ("Sorry - could not find this installations API files.\n");
+ }
+
+ function lang($str,$m1="",$m2="",$m3="") {
+   return $str;
+ }
+ function debug ($level,$msg) {
+   return TRUE;
+ }
+
+ $search = new imdbsearch();
+ $imdbmov = new imdb('0119177');
+ $imdbmov->imdbsite = "uk.imdb.com";
+
+#==========================================================[ Check Process ]===
+#---------------------------------------------------------[ Update IMDB ID ]---
+ $upd_id = 0;
+ if ($cleaninst->imdbid) {
+   $db->query("SELECT id FROM cat WHERE name IN ('$ignore_cat')");
+   while ($db->next_record()) {
+     $skip_cat .= ",".$db->f('id');
+   }
+   $query = "SELECT id FROM pvp_video WHERE imdb_id IS NULL";
+   if ($skip_cat) $query .= " AND cat1_id NOT IN ('".substr($skip_cat,1)."')";
+   $db->query($query);
+   while ($db->next_record()) {
+     $mid[] = $db->f('id');
+   }
+   $midc = count($mid);
+   if ($midc) {
+     echo "- Try to update IMDB ID for $midc movies...\n";
+     for ($i=$skip_to;$i<$midc;++$i) {
+       if ($i-$skip_to>$stopafter) break;
+       if ($i==$stop_at) break;
+       if (in_array($i,$skip_id)) continue;
+       $movie = $db->get_movie($mid[$i]);
+       $search->setsearchname($movie["title"]);
+       $results = $search->results();
+       foreach ($results as $res) {
+         if ($movie["year"]==$res->year()) {
+           if ($movie["title"]==$res->title()) {
+             echo "* [$i] MATCH for '".$movie["title"]."' (".$movie["year"]."): ".$res->title()." (".$res->year().")\n";
+             if ($write_yt) {
+               $db->query("UPDATE pvp_video SET imdb_id='".$res->imdbid()."' WHERE id=".$mid[$i]);
+               ++$upd_id;
+             }
+             break;
+           } else { // check AKAs
+             $imdbid = $res->imdbid();
+             $imdbmov->setid($imdbid);
+             $akas   = $imdbmov->alsoknow();
+             $akac   = count($akas);
+             echo "* [$i] Possible match for '".$movie["title"]."' (".$movie["year"]."): ".$res->title()." (".$res->year()."), IMDB ID '".$res->imdbid()."' ($akac AKAs)\n";
+             if (is_array($akas)) foreach ( $akas as $ak) {
+               if ($ak["title"]==$movie["title"]) {
+                 echo "* [$i] MATCH with AKA: ".$ak["title"]." (".$ak["year"].")\n";
+                 if ($write_aka_yt) {
+                   $db->query("UPDATE pvp_video SET imdb_id='$imdbid' WHERE id=".$mid[$i]);
+                   ++$upd_id;
+                   continue 3;
+                 }
+               }
+             }
+             if ($write_yf) {
+               $db->query("UPDATE pvp_video SET imdb_id='".$res->imdbid()."' WHERE id=".$mid[$i]);
+               ++$upd_id;
+               continue 2;
+             }
+             // check for director
+             unset($imdbmov); // re-initialize - otherwise breaks after the first check whyever
+             $imdbmov = new imdb($imdbid);
+             $imdbmov->imdbsite = "uk.imdb.com";
+             $dir = $imdbmov->director(); // check for director
+             for ($k=0;$k<count($dir);++$k) {
+               echo "~ [$i] DirectorCheck: '".$dir[$k]["name"]."'/'".$movie["director"]."'\n";
+               if ($movie["director"]==$dir[$k]["name"]) {
+                 echo "* [$i] MATCH with director (".$dir[$k]["name"]."): '".$movie["title"]."' (".$movie["year"]."): ".$res->title()." (".$res->year()."), IMDB ID '".$res->imdbid()."'\n";
+                 if ($write_yd) {
+                   $db->query("UPDATE pvp_video SET imdb_id='".$res->imdbid()."' WHERE id=".$mid[$i]);
+                   ++$upd_id;
+                 }
+                 continue 3;
+               }
+             }
+           }
+         }
+       }
+     }
+   } else {
+     echo "- No IMDB IDs to update.\n";
+   }
+ }
+
+#----------------------------------------------------[ Update movie rating ]---
+ $upd_rat = 0;
+ unset($mid); $midc = 0;
+ if ($cleaninst->rating) {
+   $db->query("SELECT id,imdb_id FROM pvp_video WHERE rating IS NULL AND imdb_id IS NOT NULL AND imdb_id!=''");
+   while ($db->next_record()) {
+     $m->id = $db->f('id');
+     $m->imdbid = $db->f('imdb_id');
+     $mid[] = $m;
+   }
+   $midc = count($mid);
+   if ($midc) {
+     echo "- Try to update rating for $midc movies...\n";
+     for ($i=0;$i<$midc;++$i) {
+       if ($i>$stopafter) break;
+       $imdbmov->setid($mid[$i]->imdbid);
+       $rating = $imdbmov->rating();
+       if (!empty($rating)) {
+         echo "* [$i] Rating for '".$imdbmov->title()."' (".$mid[$i]->imdbid.") is ".$rating." - Updating DB.\n";
+         $db->query("UPDATE pvp_video SET rating='$rating' WHERE id=".$mid[$i]->id);
+         ++$upd_rat;
+       }
+     }
+   } else {
+     echo "- No ratings to update.\n";
+   }
+ }
+
+#---------------------------------------------------------------[ All Done ]---
+ echo "Done - updated $upd_id IMDB IDs and $upd_rat ratings in your database.\n";
+?>
---
+++ get_name_ids.php
@@ -0,0 +1,196 @@
+#!/usr/bin/php
+<?
+#==============================================================================
+# phpVideoPro IMDB Updater for staff IDs            (c) 2009 by Itzchak Rehberg
+# Pass the name of the installation as first argument
+# -----------------------------------------------------------------------------
+# $Id$
+#==============================================================================
+
+error_reporting(E_ERROR); # disable all but errors to be reported
+
+#==========================================================[ Configuration ]===
+ $instance["demo"]->dir = '/var/www/phpvideo';
+ $instance["demo"]->adminID = 1;
+
+ $stopafter   = 500; // process no more than 200 entries at once
+ $ignore_cat  = "cat_videoclip"; // list of cat_id to ignore - like VideoClips, comma-separated
+ $skip_id     = array();
+ $skip_to     = 1; // start at the beginning or other given index
+ $stop_at     = 100;
+ $report_nochange = TRUE;
+ $compare_mode = "equal"; // how to compare: equal|binary|binary_i|soundex|metaphone
+
+ # DB, when...
+ $update_name  = FALSE; // same name, different IMDB ID - this COULD be a different person, but also an alias
+
+#=========================================================[ Initialization ]===
+ if (empty($instance[$argv[1]]->dir)) { // no such installation
+   die("Sorry - but we have no instance with the name '".$argv[1]."'.\n");
+ } else {
+   $cleaninst = $instance[$argv[1]];
+   echo "Updating PVP Installation '".$argv[1]."':\n";
+ }
+ if (file_exists($cleaninst->dir)) chdir($cleaninst->dir);
+ else die ("Sorry - but the given directory does not exist: '".$cleaninst->dir."'.\n");
+ $pvp->auth->user_id = $cleaninst->adminID;
+ $pvpinstall = 1;
+ if (file_exists("inc/includes.inc")) {
+   include ("inc/config.inc");
+   include ("inc/config_internal.inc");
+   include ("inc/class.preferences.inc");
+   $pvp->preferences = new preferences;
+   $incpath = explode(":", ini_get('include_path'));
+   $IMDBfound = FALSE;
+   foreach($incpath as $trypath) {
+     if (file_exists("$trypath/imdb.class.php")) {
+       echo "- Including IMDB class\n";
+       if (file_exists("$trypath/imdb_person.class.php")) {
+         include_once("$trypath/imdb.class.php");
+         include_once("$trypath/imdb_person.class.php");
+         $IMDBfound = TRUE;
+       }
+       break;
+     }
+   }
+   if (!$IMDBfound) die("IMDB classes not found, aborting.\n");
+ } else {
+   die ("Sorry - could not find this installations API files.\n");
+ }
+
+ function lang($str,$m1="",$m2="",$m3="") {
+   return $str;
+ }
+ function debug ($level,$msg) {
+   return TRUE;
+ }
+
+ $imdbper = new imdb_person('0000007');
+ $imdbmov = new imdb('0119177');
+ $imdbmov->imdbsite = "akas.imdb.com";
+
+#==========================================================[ Check Process ]===
+ $upd_id = 0; // counter
+
+#-------------------------------------------------[ Helper: String Compare ]---
+function samestr($str1,$str2) {
+  if (in_array($GLOBALS["compare_mode"],array("soundex","metaphone"))) {
+    $str1 = str_replace("ß","ss",$str1);
+    $str2 = str_replace("ß","ss",$str2);
+  }
+  switch($GLOBALS["compare_mode"]) {
+    case "binary"    : return (strcmp(trim($str1),trim($str2))==0); // case sensitive identical should consider locale - but does not ("ß"!="ss", "e"!="é")
+    case "binary_i"  : return (strcasecmp(trim($str1),trim($str2))==0); // case insensitive identical - does not work ("ß"!="ss", "e"!="é")
+    case "metaphone" : return (metaphone($str1)==metaphone($str2)); // improved soundex compare (more identical, see example below) - "e"="é", but "ss"!="ß"
+    case "soundex"   : return (soundex($str1)==soundex($str2)); // like metaphone, but faster -- but "Hilbert"="Heilbronn" (urgs)
+    case "equal"     : 
+    default          : return ($str1==$str2);          // absolute identical
+  }
+}
+
+#-----------------------------------------------------[ Helper: Update IDs ]---
+function imdbCheck(&$iactors,&$actor,$role,$table) {
+  GLOBAL $upd_id, $pfound, $db, $movie;
+  foreach($iactors as $iactor) { // walk the IMDB data set to find matches
+    $pfound = FALSE;
+    if (empty($actor['imdb_id'])) {
+      if (samestr($iactor['name'],$actor['fullname'])) { // got her/him obviously
+        $db->query("UPDATE $table SET imdb_id='".$iactor['imdb']."' WHERE id=".$actor['id']);
+        echo "  + Updated $role '".$actor['fullname']."' (ID ".$actor['id'].") with IMDB ID '".$iactor['imdb']."'\n";
+        ++$upd_id;
+        $pfound = TRUE;
+        break;
+      }
+    } else {
+      if (samestr($iactor['imdb'],$actor['imdb_id'])) { // already OK
+        if ($report_nochange) echo "  - $role '".$actor['fullname']."' is already stored with the correct ID (".$iactor['imdb'].")\n";
+        $pfound = TRUE;
+        break;
+      }
+      if (samestr($iactor['name'],$actor['fullname'])) { // same name, different id - shit happens!
+        $pfound = TRUE;
+        if (!$update_name) break;
+        echo "  ! $role '".$actor['fullname']."' is known at IMDB by ID '".$iactor['imdb']."', but we have '".$actor['imdb_id'].":'\n";
+        // here we have to create a new DB entry for the given person, and update the movie record accordingly:
+        $aid = $db->insert_person($role,"SELECT id FROM $table","INSERT INTO $table",$actor['name'],$actor['firstname'],$iactor['imdb']);
+        if ($aid) {
+          echo "   - Created new record with ID ${aid}\n";
+          if ($role=="actor") {
+            $db->query("SELECT actor1_id,actor2_id,actor3_id,actor4_id,actor5_id FROM pvp_video WHERE id=".$movie['id']);
+            $db->next_record();
+            for ($i=1;$i<6;++$i) {
+              $xid = $db->f("actor${i}_id");
+              if ($xid==$actor['id']) {
+                $db->query("UPDATE pvp_video SET actor${i}_id=$aid WHERE id=".$movie['id']);
+                echo "   + Updated record of movie id '".$movie['id']."' for actor $i (".$actor['id'].") to actor ID '$aid'\n";
+                break;
+              }
+            }
+          } else {
+            $db->query("UPDATE pvp_video SET ${role}_id=$aid WHERE id=".$movie['id']);
+            echo "   + Updated record of movie id '".$movie['id']."' for $role (".$actor['id'].") to $role ID '$aid'\n";
+          }
+        }
+      }
+    }
+  }
+}
+
+#------------------------------------------------[ Gather the movies first ]---
+ if (!empty($ignore_cat)) {
+   $skip_cat = "";
+   $db->query("SELECT id FROM cat WHERE name IN ('$ignore_cat')");
+   while ($db->next_record()) {
+     $skip_cat .= ",".$db->f('id');
+   }
+ }
+ // we need the movies imdb id to verify that we get the right person
+ $query = "SELECT id FROM pvp_video WHERE imdb_id IS NOT NULL";
+ if (isset($skip_cat)) $query .= " AND cat1_id NOT IN ('".substr($skip_cat,1)."')";
+ $db->query($query);
+ while ($db->next_record()) {
+   $mid[] = $db->f('id');
+ }
+ $midc = count($mid);
+#-----------------------------------------------[ Go for the staff members ]---
+ if ($midc) {
+   echo "- Try to update staff members for $midc movies...\n";
+   for ($i=$skip_to;$i<$midc;++$i) { // walk the movie array
+     if ($i-$skip_to>$stopafter) break;
+     if ($i==$stop_at) break;
+     if (in_array($i,$skip_id)) continue;
+     $movie = $db->get_movie($mid[$i]);
+     echo " - Processing movie ID '".$mid[$i]."' (".$movie["title"].")\n";
+     $imdbmov->setid($movie["imdb_id"]);
+     $iactors = $imdbmov->cast(); $iac = count($iactors);
+     if ($iac==0) { // not found in IMDB - ooops? - or no actors found there
+       echo "  ! Sorry, no actors recorded for this movie at the IMDB site\n";
+       continue;
+     }
+     for ($k=1;$k<6;++$k) { // actors
+       $idn = "actor${k}_id";
+       $actor = $db->get_actor($movie[$idn]);
+       if (empty($actor['fullname'])) continue; // not set
+       imdbCheck($iactors,$actor,"actor","actors");
+       if (!$pfound) echo "  ! actor '".$actor['fullname']."' (ID ".$actor['id'].") was not found in connection with this movie at IMDB.com - not updated.\n";
+     } // end internal actor walk (for $k)
+     if (!empty($movie['director_id'])) { // director
+       $actor = $db->get_director($movie['director_id']);
+       $iactors = $imdbmov->director();
+       imdbCheck($iactors,$actor,"director","directors");
+       if (!$pfound) echo "  ! director '".$actor['fullname']."' (ID ".$actor['id'].") was not found in connection with this movie at IMDB.com - not updated.\n";
+     }
+     if (!empty($movie['music_id'])) { // music
+       $actor = $db->get_music($movie['music_id']);
+       $iactors = $imdbmov->composer();
+       imdbCheck($iactors,$actor,"componist","music");
+       if (!$pfound) echo "  ! componist '".$actor['fullname']."' (ID ".$actor['id'].") was not found in connection with this movie at IMDB.com - not updated.\n";
+     }
+   } // end movie array walk (for $i)
+ } else {
+   echo "- No IMDB IDs to update.\n";
+ }
+
+#---------------------------------------------------------------[ All Done ]---
+ echo "Done - updated $upd_id IMDB IDs of crew members in your database.\n";
+?>
